c90b217d3bab89e3dd3683ffdb5905914c4f4f1d
Saurab Rajbanshi <saurab57322@gmail.com>
2024-12-27 21:25:22 +0545

    initial commit

---
+++ RazorViews/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace RazorViews.Models
+{
+	public class Person
+	{
+		public string? PersonName { get; set; }
+		public DateTime? DateOfBirth { get; set; }
+		public Gender PersonGender { get; set; }
+	}
+	public enum Gender
+	{
+		Male, Female, Other
+	}
+}
